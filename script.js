936fddc7fbc802b11d162501288b5e6784986ee4
matewkillin <matewkillin@gmail.com>
2024-06-16 20:20:07 +0200

    first commit

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ function generateCards(places) {
     const container = document.getElementById('cards-container');
     container.innerHTML = '';
     places.forEach(place => {
-        // Corregir la ruta de la imagen local
         const localImageUrl = place.localImageUrl ? place.localImageUrl.replace(/\\/g, '/').replace('D:/Code/Scrapping BCN/', '') : '';
 
         const card = document.createElement('div');
         card.className = 'card';
         card.setAttribute('data-category', place.category);
-        
+
         card.innerHTML = `
             ${localImageUrl ? `<img src="${localImageUrl}" alt="${place.name}">` : ''}
             <h3>${place.name}</h3>
@@ -35,6 +34,20 @@ function filterCategory(category) {
     });
 
     document.querySelector(`.nav-bar button[onclick="filterCategory('${category}')"]`).classList.add('selected');
+}
+
+function toggleDropdown() {
+    const dropdown = document.querySelector('.dropdown-content');
+    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+}
+
+function loadPage(url) {
+    const contentContainer = document.getElementById('content-container');
+    if (url.endsWith('.html')) {
+        window.location.href = url;
+    } else {
+        contentContainer.innerHTML = `<iframe src="${url}" width="100%" height="100%" style="border:none;"></iframe>`;
+    }
 }
 
 window.onload = function() {
